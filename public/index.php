e2d48d87df359228ee856d534c7a280db95b8aeb
kawanamiyuu <i.am.really.you@gmail.com>
2020-06-12 08:27:41 +0900

    refactor: instantiate by constructor

--- public/index.php
+++ public/index.php
@@ -6,24 +6,17 @@ use Kawanamiyuu\HtbFeed\Http\ErrorHandler;
 use Kawanamiyuu\HtbFeed\Http\ResponseEmitter;
 use Kawanamiyuu\HtbFeed\Http\RequestHandler;
 use Ray\Compiler\ScriptInjector;
-use Ray\Di\Bind;
-use Ray\Di\InjectorInterface;
 
 define('ROOT_DIR', dirname(__DIR__));
 
 require ROOT_DIR . '/bootstrap/bootstrap.php';
 require ROOT_DIR . '/vendor/autoload.php';
 
-$injector = new ScriptInjector(ROOT_DIR . '/var/tmp', function () use (&$injector) {
-    $module = new AppModule();
-    (new Bind($module->getContainer(), InjectorInterface::class))->toInstance($injector);
-    return $module;
+$injector = new ScriptInjector(ROOT_DIR . '/var/tmp', function () {
+    return new AppModule();
 });
 
-$app = $injector->getInstance(Application::class);
-/* @var Application $app */
-
-$app->run([
+(new Application($injector))->run([
     ResponseEmitter::class,
     ErrorHandler::class,
     RequestHandler::class
